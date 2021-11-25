ca30810b42508156e2ff28d5f58bc033b7d9cd7a
Troels F. Roennow <troels@wonop.com>
2021-11-25 15:22:56 +0100

    Updating style

--- qir/qat/RemoveDisallowedAttributesPass/RemoveDisallowedAttributesPass.hpp
+++ qir/qat/RemoveDisallowedAttributesPass/RemoveDisallowedAttributesPass.hpp
@@ -2,85 +2,89 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-#include "Llvm/Llvm.hpp"
 #include "QatTypes/QatTypes.hpp"
+
+#include "Llvm/Llvm.hpp"
 
 #include <functional>
 #include <unordered_set>
 #include <vector>
 
-namespace microsoft {
-namespace quantum {
-
-class RemoveDisallowedAttributesPass : public llvm::PassInfoMixin<RemoveDisallowedAttributesPass>
+namespace microsoft
 {
-public:
-  RemoveDisallowedAttributesPass()
-    : allowed_attrs_{{static_cast<String>("EntryPoint"), static_cast<String>("InteropFriendly")}}
-  {}
+namespace quantum
+{
 
-  llvm::PreservedAnalyses run(llvm::Module &module, llvm::ModuleAnalysisManager & /*mam*/)
-  {
-    std::string const LLVM_FUNC_NAME = "@llvm.";
-    for (auto &fnc : module)
+    class RemoveDisallowedAttributesPass : public llvm::PassInfoMixin<RemoveDisallowedAttributesPass>
     {
-      std::unordered_set<String> to_keep;
-      auto                       name = static_cast<std::string>(fnc.getName());
-
-      // Skipping any LLVM function
-      /*
-      if (name.size() >= LLVM_FUNC_NAME.size() &&
-          name.substr(0, LLVM_FUNC_NAME.size()) == LLVM_FUNC_NAME)
-      {
-        continue;
-      }
-      */
-      // Finding all valid attributes
-      for (auto &attrset : fnc.getAttributes())
-      {
-        for (auto &attr : attrset)
+      public:
+        RemoveDisallowedAttributesPass()
+          : allowed_attrs_{{static_cast<String>("EntryPoint"), static_cast<String>("InteropFriendly")}}
         {
-          auto r = static_cast<String>(attr.getAsString());
-
-          // Stripping quotes
-          if (r.size() >= 2 && r[0] == '"' && r[r.size() - 1] == '"')
-          {
-            r = r.substr(1, r.size() - 2);
-          }
-
-          // Inserting if allowed
-          if (allowed_attrs_.find(r) != allowed_attrs_.end())
-          {
-            to_keep.insert(r);
-          }
         }
-      }
 
-      // Deleting every
-      fnc.setAttributes({});
-      for (auto &attr : to_keep)
-      {
-        fnc.addFnAttr(attr);
-      }
-      /*
-      // Updating all users attributes
-      for (auto user : fnc.users())
-      {
-        auto call = llvm::dyn_cast<llvm::CallInst>(user);
-        if (call != nullptr)
+        llvm::PreservedAnalyses run(llvm::Module& module, llvm::ModuleAnalysisManager& /*mam*/)
         {
-          call->setAttributes(fnc.getAttributes());
+            std::string const LLVM_FUNC_NAME = "@llvm.";
+            for (auto& fnc : module)
+            {
+                std::unordered_set<String> to_keep;
+                auto                       name = static_cast<std::string>(fnc.getName());
+
+                // Skipping any LLVM function
+                /*
+                if (name.size() >= LLVM_FUNC_NAME.size() &&
+                    name.substr(0, LLVM_FUNC_NAME.size()) == LLVM_FUNC_NAME)
+                {
+                  continue;
+                }
+                */
+                // Finding all valid attributes
+                for (auto& attrset : fnc.getAttributes())
+                {
+                    for (auto& attr : attrset)
+                    {
+                        auto r = static_cast<String>(attr.getAsString());
+
+                        // Stripping quotes
+                        if (r.size() >= 2 && r[0] == '"' && r[r.size() - 1] == '"')
+                        {
+                            r = r.substr(1, r.size() - 2);
+                        }
+
+                        // Inserting if allowed
+                        if (allowed_attrs_.find(r) != allowed_attrs_.end())
+                        {
+                            to_keep.insert(r);
+                        }
+                    }
+                }
+
+                // Deleting every
+                fnc.setAttributes({});
+                for (auto& attr : to_keep)
+                {
+                    fnc.addFnAttr(attr);
+                }
+                /*
+                // Updating all users attributes
+                for (auto user : fnc.users())
+                {
+                  auto call = llvm::dyn_cast<llvm::CallInst>(user);
+                  if (call != nullptr)
+                  {
+                    call->setAttributes(fnc.getAttributes());
+                  }
+                }
+                */
+            }
+
+            return llvm::PreservedAnalyses::none();
         }
-      }
-      */
-    }
 
-    return llvm::PreservedAnalyses::none();
-  }
+      private:
+        std::unordered_set<String> allowed_attrs_;
+    };
 
-private:
-  std::unordered_set<String> allowed_attrs_;
-};
-
-}  // namespace quantum
-}  // namespace microsoft
+} // namespace quantum
+} // namespace microsoft
