7b9af0850993051a92137bf491265ef4dbbe651e
Troels F. Roennow <troels@wonop.com>
2022-05-12 11:46:37 +0200

    Updating pass

---
+++ qir/qat/PreTransformTrimming/PreTransformTrimmingPassConfiguration.hpp
@@ -0,0 +1,39 @@
+#pragma once
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include "Commandline/ConfigurationManager.hpp"
+
+#include <limits>
+
+namespace microsoft {
+namespace quantum {
+
+class PreTransformTrimmingPassConfiguration
+{
+public:
+  using DeferredValuePtr = DeferredValue::DeferredValuePtr;
+
+  void setup(ConfigurationManager &config)
+  {
+    config.setSectionName("Pre-transform trimming", "");
+    entry_point_attr_ = config.getParameter("entry-point-attr");
+  }
+
+  static PreTransformTrimmingPassConfiguration createDisabled()
+  {
+    PreTransformTrimmingPassConfiguration ret;
+    return ret;
+  }
+
+  std::string entryPointAttr() const
+  {
+    return entry_point_attr_->value<std::string>();
+  }
+
+private:
+  DeferredValuePtr entry_point_attr_{nullptr};
+};
+
+}  // namespace quantum
+}  // namespace microsoft
