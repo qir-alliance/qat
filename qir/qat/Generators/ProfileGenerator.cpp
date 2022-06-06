c4e57977d1b7d78947c38dfc690276ba744e9b8f
Troels F. Roennow <troels@wonop.com>
2022-06-06 18:46:24 +0200

    Fixing CI

--- qir/qat/Generators/ProfileGenerator.cpp
+++ qir/qat/Generators/ProfileGenerator.cpp
@@ -18,61 +18,15 @@
 #include "StaticResourceComponent/StaticResourceComponentConfiguration.hpp"
 #include "TransformationRulesPass/TransformationRulesPass.hpp"
 #include "TransformationRulesPass/TransformationRulesPassConfiguration.hpp"
+#include "Utils/FunctionToModule.hpp"
 #include "ValidationPass/ValidationPassConfiguration.hpp"
 
 #include "Llvm/Llvm.hpp"
+
 namespace microsoft
 {
 namespace quantum
 {
-
-    class FunctionToModule : public llvm::PassInfoMixin<FunctionToModule>
-    {
-      public:
-        // Construction and destruction configuration.
-        //
-
-        FunctionToModule(llvm::FunctionPassManager&& manager)
-          : manager_{std::move(manager)}
-        {
-        }
-
-        /// Copy construction is banned.
-        FunctionToModule(FunctionToModule const&) = delete;
-
-        /// We allow move semantics.
-        FunctionToModule(FunctionToModule&&) = default;
-
-        /// Default destruction.
-        ~FunctionToModule() = default;
-        llvm::PreservedAnalyses run(llvm::Module& module, llvm::ModuleAnalysisManager& mam)
-        {
-            llvm::FunctionAnalysisManager& fam =
-                mam.getResult<llvm::FunctionAnalysisManagerModuleProxy>(module).getManager();
-
-            for (auto& f : module)
-            {
-                if (f.isDeclaration())
-                {
-                    continue;
-                }
-
-                auto ret = manager_.run(f, fam);
-                fam.invalidate(f, ret);
-            }
-
-            return llvm::PreservedAnalyses::none();
-        }
-        /// Whether or not this pass is required to run.
-        static bool isRequired()
-        {
-            return true;
-        }
-
-      private:
-        llvm::FunctionPassManager manager_;
-    };
-
     Profile ProfileGenerator::newProfile(String const& name, OptimizationLevel const& optimization_level, bool debug)
     {
         auto qubit_allocation_manager  = BasicAllocationManager::createNew();
