f962333269e3ee772c04a2355d81bd6b41ff5038
Troels F. Roennow <troels@wonop.com>
2022-06-07 09:17:22 +0200

    Updating style

--- qir/qat/ModuleLoader/ModuleLoader.hpp
+++ qir/qat/ModuleLoader/ModuleLoader.hpp
@@ -2,180 +2,182 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-#include "Llvm/Llvm.hpp"
 #include "ModuleLoader/DebugInfoUpdater.hpp"
 #include "ModuleLoader/InstructionLocationTable.hpp"
 #include "QatTypes/QatTypes.hpp"
 #include "RemoveDisallowedAttributesPass/RemoveDisallowedAttributesPass.hpp"
 
-namespace microsoft {
-namespace quantum {
+#include "Llvm/Llvm.hpp"
 
-class ModuleLoader
+namespace microsoft
 {
-public:
-  using Module       = llvm::Module;
-  using Linker       = llvm::Linker;
-  using SMDiagnostic = llvm::SMDiagnostic;
+namespace quantum
+{
 
-  explicit ModuleLoader(Module *final_module, bool strip_existing_debug = false,
-                        bool add_ir_debug_info = false)
-    : final_module_{final_module}
-    , linker_{*final_module}
-    , instruction_location_table_{InstructionLocationTable::create()}
-    , strip_existing_debug_{strip_existing_debug}
-    , add_ir_debug_info_{add_ir_debug_info}
-  {}
-
-  bool addModule(std::unique_ptr<Module> &&module, String const &filename = "unknown")
-  {
-    if (llvm::verifyModule(*module, &llvm::errs()))
+    class ModuleLoader
     {
-      llvm::errs() << filename << ": "
-                   << "input module is broken!\n";
-      return false;
-    }
+      public:
+        using Module       = llvm::Module;
+        using Linker       = llvm::Linker;
+        using SMDiagnostic = llvm::SMDiagnostic;
 
-    return !linker_.linkInModule(std::move(module), Linker::Flags::None);
-  }
+        explicit ModuleLoader(Module* final_module, bool strip_existing_debug = false, bool add_ir_debug_info = false)
+          : final_module_{final_module}
+          , linker_{*final_module}
+          , instruction_location_table_{InstructionLocationTable::create()}
+          , strip_existing_debug_{strip_existing_debug}
+          , add_ir_debug_info_{add_ir_debug_info}
+        {
+        }
 
-  bool addIrFile(String input_file)
-  {
-    // Converting to absolute path
-    llvm::SmallVector<char, 256> input_vec;
-    input_vec.assign(input_file.begin(), input_file.end());
-    llvm::sys::fs::make_absolute(input_vec);
-    input_file.resize(input_vec.size());
-    uint64_t i = 0;
-    for (auto &s : input_vec)
-    {
-      input_file[i++] = s;
-    }
+        bool addModule(std::unique_ptr<Module>&& module, String const& filename = "unknown")
+        {
+            if (llvm::verifyModule(*module, &llvm::errs()))
+            {
+                llvm::errs() << filename << ": "
+                             << "input module is broken!\n";
+                return false;
+            }
 
-    // Loading module
-    SMDiagnostic            err;
-    std::unique_ptr<Module> module =
-        llvm::parseIRFile(input_file, err, final_module_->getContext());
-    if (!module)
-    {
-      llvm::errs() << "Failed to load " << input_file << "\n";
-      return false;
-    }
+            return !linker_.linkInModule(std::move(module), Linker::Flags::None);
+        }
 
-    // Registering all debug info
-    auto directory = llvm::sys::path::parent_path(input_file);
-    auto filename  = llvm::sys::path::filename(input_file);
+        bool addIrFile(String input_file)
+        {
+            // Converting to absolute path
+            llvm::SmallVector<char, 256> input_vec;
+            input_vec.assign(input_file.begin(), input_file.end());
+            llvm::sys::fs::make_absolute(input_vec);
+            input_file.resize(input_vec.size());
+            uint64_t i = 0;
+            for (auto& s : input_vec)
+            {
+                input_file[i++] = s;
+            }
 
-    instruction_location_table_->registerModule(input_file, module.get());
+            // Loading module
+            SMDiagnostic            err;
+            std::unique_ptr<Module> module = llvm::parseIRFile(input_file, err, final_module_->getContext());
+            if (!module)
+            {
+                llvm::errs() << "Failed to load " << input_file << "\n";
+                return false;
+            }
 
-    if (strip_existing_debug_)
-    {
-      // Debug info
-      llvm::StripDebugInfo(*module.get());
-    }
+            // Registering all debug info
+            auto directory = llvm::sys::path::parent_path(input_file);
+            auto filename  = llvm::sys::path::filename(input_file);
 
-    // Whether or not to override debug symbols
-    if (add_ir_debug_info_)
-    {
+            instruction_location_table_->registerModule(input_file, module.get());
 
-      // Adding debug versioning
-      auto debug_version_key = "Debug Info Version";
-      if (!module->getModuleFlag(debug_version_key))
-      {
-        module->addModuleFlag(llvm::Module::Warning, debug_version_key,
-                              llvm::DEBUG_METADATA_VERSION);
-      }
+            if (strip_existing_debug_)
+            {
+                // Debug info
+                llvm::StripDebugInfo(*module.get());
+            }
 
-      // Update with debug information
-      DebugInfoUpdater updater(instruction_location_table_, *module.get(), directory, filename);
-      updater.update();
-    }
+            // Whether or not to override debug symbols
+            if (add_ir_debug_info_)
+            {
 
-    // Transforming module
-    SingleModuleTransformation transformation;
-    if (!transformation.apply(module.get()))
-    {
-      llvm::errs() << "Failed to prepare " << input_file
-                   << " using single module transformations.\n";
-      return false;
-    }
+                // Adding debug versioning
+                auto debug_version_key = "Debug Info Version";
+                if (!module->getModuleFlag(debug_version_key))
+                {
+                    module->addModuleFlag(llvm::Module::Warning, debug_version_key, llvm::DEBUG_METADATA_VERSION);
+                }
 
-    // Linking
-    return addModule(std::move(module), input_file);
-  }
+                // Update with debug information
+                DebugInfoUpdater updater(instruction_location_table_, *module.get(), directory, filename);
+                updater.update();
+            }
 
-  InstructionLocationTablePtr locationTable() const
-  {
-    return instruction_location_table_;
-  }
+            // Transforming module
+            SingleModuleTransformation transformation;
+            if (!transformation.apply(module.get()))
+            {
+                llvm::errs() << "Failed to prepare " << input_file << " using single module transformations.\n";
+                return false;
+            }
 
-private:
-  Module                     *final_module_;
-  Linker                      linker_;
-  InstructionLocationTablePtr instruction_location_table_{nullptr};
-  bool                        strip_existing_debug_{false};
-  bool                        add_ir_debug_info_{false};
+            // Linking
+            return addModule(std::move(module), input_file);
+        }
 
-  // Single Module Transformation
-  //
+        InstructionLocationTablePtr locationTable() const
+        {
+            return instruction_location_table_;
+        }
 
-  class SingleModuleTransformation
-  {
-  public:
-    using PassBuilder             = llvm::PassBuilder;
-    using OptimizationLevel       = PassBuilder::OptimizationLevel;
-    using FunctionAnalysisManager = llvm::FunctionAnalysisManager;
+      private:
+        Module*                     final_module_;
+        Linker                      linker_;
+        InstructionLocationTablePtr instruction_location_table_{nullptr};
+        bool                        strip_existing_debug_{false};
+        bool                        add_ir_debug_info_{false};
 
-    explicit SingleModuleTransformation(
-        OptimizationLevel const &optimization_level = OptimizationLevel::O0, bool debug = false)
-      : loop_analysis_manager_{}
-      , function_analysis_manager_{}
-      , gscc_analysis_manager_{}
-      , module_analysis_manager_{}
-      , optimization_level_{optimization_level}
-      , debug_{debug}
-    {
+        // Single Module Transformation
+        //
 
-      pass_builder_.registerModuleAnalyses(module_analysis_manager_);
-      pass_builder_.registerCGSCCAnalyses(gscc_analysis_manager_);
-      pass_builder_.registerFunctionAnalyses(function_analysis_manager_);
-      pass_builder_.registerLoopAnalyses(loop_analysis_manager_);
+        class SingleModuleTransformation
+        {
+          public:
+            using PassBuilder             = llvm::PassBuilder;
+            using OptimizationLevel       = PassBuilder::OptimizationLevel;
+            using FunctionAnalysisManager = llvm::FunctionAnalysisManager;
 
-      pass_builder_.crossRegisterProxies(loop_analysis_manager_, function_analysis_manager_,
-                                         gscc_analysis_manager_, module_analysis_manager_);
+            explicit SingleModuleTransformation(
+                OptimizationLevel const& optimization_level = OptimizationLevel::O0,
+                bool                     debug              = false)
+              : loop_analysis_manager_{}
+              , function_analysis_manager_{}
+              , gscc_analysis_manager_{}
+              , module_analysis_manager_{}
+              , optimization_level_{optimization_level}
+              , debug_{debug}
+            {
 
-      module_pass_manager_.addPass(RemoveDisallowedAttributesPass());
-    }
+                pass_builder_.registerModuleAnalyses(module_analysis_manager_);
+                pass_builder_.registerCGSCCAnalyses(gscc_analysis_manager_);
+                pass_builder_.registerFunctionAnalyses(function_analysis_manager_);
+                pass_builder_.registerLoopAnalyses(loop_analysis_manager_);
 
-    bool apply(llvm::Module *module)
-    {
-      module_pass_manager_.run(*module, module_analysis_manager_);
+                pass_builder_.crossRegisterProxies(
+                    loop_analysis_manager_, function_analysis_manager_, gscc_analysis_manager_,
+                    module_analysis_manager_);
 
-      if (llvm::verifyModule(*module, &llvm::errs()))
-      {
-        return false;
-      }
+                module_pass_manager_.addPass(RemoveDisallowedAttributesPass());
+            }
 
-      return true;
-    }
+            bool apply(llvm::Module* module)
+            {
+                module_pass_manager_.run(*module, module_analysis_manager_);
 
-    bool isDebugMode() const
-    {
-      return debug_;
-    }
+                if (llvm::verifyModule(*module, &llvm::errs()))
+                {
+                    return false;
+                }
 
-  private:
-    llvm::PassBuilder             pass_builder_;
-    llvm::LoopAnalysisManager     loop_analysis_manager_;
-    llvm::FunctionAnalysisManager function_analysis_manager_;
-    llvm::CGSCCAnalysisManager    gscc_analysis_manager_;
-    llvm::ModuleAnalysisManager   module_analysis_manager_;
+                return true;
+            }
 
-    llvm::ModulePassManager module_pass_manager_{};
-    OptimizationLevel       optimization_level_{};
-    bool                    debug_{false};
-  };
-};
+            bool isDebugMode() const
+            {
+                return debug_;
+            }
 
-}  // namespace quantum
-}  // namespace microsoft
+          private:
+            llvm::PassBuilder             pass_builder_;
+            llvm::LoopAnalysisManager     loop_analysis_manager_;
+            llvm::FunctionAnalysisManager function_analysis_manager_;
+            llvm::CGSCCAnalysisManager    gscc_analysis_manager_;
+            llvm::ModuleAnalysisManager   module_analysis_manager_;
+
+            llvm::ModulePassManager module_pass_manager_{};
+            OptimizationLevel       optimization_level_{};
+            bool                    debug_{false};
+        };
+    };
+
+} // namespace quantum
+} // namespace microsoft
--- qir/qat/RemoveDisallowedAttributesPass/RemoveDisallowedAttributesPass.hpp
+++ qir/qat/RemoveDisallowedAttributesPass/RemoveDisallowedAttributesPass.hpp
@@ -2,112 +2,116 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-#include "Llvm/Llvm.hpp"
 #include "QatTypes/QatTypes.hpp"
+
+#include "Llvm/Llvm.hpp"
 
 #include <functional>
 #include <unordered_set>
 #include <vector>
 
-namespace microsoft {
-namespace quantum {
-
-class RemoveDisallowedAttributesPass : public llvm::PassInfoMixin<RemoveDisallowedAttributesPass>
+namespace microsoft
 {
-public:
-  RemoveDisallowedAttributesPass()
-    : allowed_attrs_{
-          static_cast<String>("EntryPoint"), static_cast<String>("InteropFriendly"),
-          static_cast<String>("requiredQubits"), static_cast<String>("requiredResults")
-          /*
-          static_cast<String>("nofree")
-          static_cast<String>("nosync")
-          static_cast<String>("nounwind")
-          static_cast<String>("readnone")
-          static_cast<String>("speculatable")
-          static_cast<String>("willreturn")
-          */
-      }
-  {}
+namespace quantum
+{
 
-  llvm::PreservedAnalyses run(llvm::Module &module, llvm::ModuleAnalysisManager & /*mam*/)
-  {
-    for (auto &fnc : module)
+    class RemoveDisallowedAttributesPass : public llvm::PassInfoMixin<RemoveDisallowedAttributesPass>
     {
-      std::unordered_map<String, String> to_keep;
-      auto                               name = static_cast<std::string>(fnc.getName());
-
-      // Skipping any LLVM function
-      if (fnc.isIntrinsic())
-      {
-        continue;
-      }
-
-      // Finding all valid attributes
-      for (auto &attrset : fnc.getAttributes())
-      {
-        for (auto &attr : attrset)
+      public:
+        RemoveDisallowedAttributesPass()
+          : allowed_attrs_{
+                static_cast<String>("EntryPoint"), static_cast<String>("InteropFriendly"),
+                static_cast<String>("requiredQubits"), static_cast<String>("requiredResults")
+                /*
+                static_cast<String>("nofree")
+                static_cast<String>("nosync")
+                static_cast<String>("nounwind")
+                static_cast<String>("readnone")
+                static_cast<String>("speculatable")
+                static_cast<String>("willreturn")
+                */
+            }
         {
-          auto        r = static_cast<String>(attr.getAsString());
-          std::string value{};
-
-          auto p = r.find('=');
-          if (p != std::string::npos)
-          {
-            value = r.substr(p + 1, r.size() - p - 1);
-            r     = r.substr(0, p);
-          }
-
-          // Stripping quotes
-          if (r.size() >= 2 && r[0] == '"' && r[r.size() - 1] == '"')
-          {
-            r = r.substr(1, r.size() - 2);
-          }
-
-          if (value.size() >= 2 && value[0] == '"' && value[value.size() - 1] == '"')
-          {
-            value = value.substr(1, value.size() - 2);
-          }
-
-          // Inserting if allowed
-          if (allowed_attrs_.find(r) != allowed_attrs_.end())
-          {
-            to_keep.insert(std::make_pair(r, value));
-          }
         }
-      }
 
-      // Deleting every
-      fnc.setAttributes({});
-      for (auto &attr : to_keep)
-      {
-        if (attr.second.empty())
+        llvm::PreservedAnalyses run(llvm::Module& module, llvm::ModuleAnalysisManager& /*mam*/)
         {
-          fnc.addFnAttr(attr.first);
+            for (auto& fnc : module)
+            {
+                std::unordered_map<String, String> to_keep;
+                auto                               name = static_cast<std::string>(fnc.getName());
+
+                // Skipping any LLVM function
+                if (fnc.isIntrinsic())
+                {
+                    continue;
+                }
+
+                // Finding all valid attributes
+                for (auto& attrset : fnc.getAttributes())
+                {
+                    for (auto& attr : attrset)
+                    {
+                        auto        r = static_cast<String>(attr.getAsString());
+                        std::string value{};
+
+                        auto p = r.find('=');
+                        if (p != std::string::npos)
+                        {
+                            value = r.substr(p + 1, r.size() - p - 1);
+                            r     = r.substr(0, p);
+                        }
+
+                        // Stripping quotes
+                        if (r.size() >= 2 && r[0] == '"' && r[r.size() - 1] == '"')
+                        {
+                            r = r.substr(1, r.size() - 2);
+                        }
+
+                        if (value.size() >= 2 && value[0] == '"' && value[value.size() - 1] == '"')
+                        {
+                            value = value.substr(1, value.size() - 2);
+                        }
+
+                        // Inserting if allowed
+                        if (allowed_attrs_.find(r) != allowed_attrs_.end())
+                        {
+                            to_keep.insert(std::make_pair(r, value));
+                        }
+                    }
+                }
+
+                // Deleting every
+                fnc.setAttributes({});
+                for (auto& attr : to_keep)
+                {
+                    if (attr.second.empty())
+                    {
+                        fnc.addFnAttr(attr.first);
+                    }
+                    else
+                    {
+                        fnc.addFnAttr(attr.first, attr.second);
+                    }
+                }
+
+                // Updating all users attributes
+                for (auto user : fnc.users())
+                {
+                    auto call = llvm::dyn_cast<llvm::CallInst>(user);
+                    if (call != nullptr)
+                    {
+                        call->setAttributes(fnc.getAttributes());
+                    }
+                }
+            }
+
+            return llvm::PreservedAnalyses::none();
         }
-        else
-        {
-          fnc.addFnAttr(attr.first, attr.second);
-        }
-      }
 
-      // Updating all users attributes
-      for (auto user : fnc.users())
-      {
-        auto call = llvm::dyn_cast<llvm::CallInst>(user);
-        if (call != nullptr)
-        {
-          call->setAttributes(fnc.getAttributes());
-        }
-      }
-    }
+      private:
+        std::unordered_set<String> allowed_attrs_;
+    };
 
-    return llvm::PreservedAnalyses::none();
-  }
-
-private:
-  std::unordered_set<String> allowed_attrs_;
-};
-
-}  // namespace quantum
-}  // namespace microsoft
+} // namespace quantum
+} // namespace microsoft
